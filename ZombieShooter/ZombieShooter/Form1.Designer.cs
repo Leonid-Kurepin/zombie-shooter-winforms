20609fcdaa8e9a0f9b023644944610028ded9b4c
Leonid Kurepin <leonidkurepin@yandex.ru>
2019-12-10 20:34:08 +0400

    Add sprites

--- ZombieShooter/ZombieShooter/Form1.Designer.cs
+++ ZombieShooter/ZombieShooter/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(921, 658);
+            this.Name = "Form1";
+            this.Text = "Zombie Shooter";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
